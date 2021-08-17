014fd0e876ac43d43130fbfc6fdb30ce313a3edb
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-16 21:02:17 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Todo = props => {
   return (
-    <li>{props.text}</li>
+    // <li>{props.text}</li>
+    <div>
+      <span>{props.todo.text}</span><button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+    </div>
   );
 };
 
